e6d1705226a679754ff51312b058af09e0b02b8c
Miller Johnston <miller.johnston17@gmail.com>
2023-10-04 23:00:28 -0700

    Rendering SDL Window and implemented a game loop to take user input

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,16 +1,11 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_ttf.h>
-#include <glm/glm.hpp>
-#include <imgui/imgui.h>
-#include <iostream>
-#include <sol/sol.hpp>
+#include "Game.h"
 
-int main() {
-  SDL_Init(SDL_INIT_EVERYTHING);
-  sol::state lua;
-  lua.open_libraries(sol::lib::base);
-  std::cout << "Hello, world! And Github, branch and PR test now" << std::endl;
+int main(int argc, char *argv[]) {
+  Game game;
+
+  game.Initialize();
+  game.Run();
+  game.Destroy();
+
   return 0;
 }
