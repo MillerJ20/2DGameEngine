eda345320cb6b82d0d12b50ff79db74af4cb00f1
Miller Johnston <miller.johnston17@gmail.com>
2023-10-07 15:04:53 -0700

    Capped the framerate in update method

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,9 +1,13 @@
 #include "Game.h"
+#include "glm/fwd.hpp"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
+#include <SDL2/SDL_image.h>
 #include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_render.h>
+#include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_video.h>
+#include <glm/glm.hpp>
 #include <iostream>
 
 Game::Game() {
@@ -42,8 +46,16 @@ void Game::Initialize() {
 
   isRunning = true;
 }
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
+
+void Game::Setup() {
+  playerPosition = glm::vec2(10.0, 20.0);
+  playerVelocity = glm::vec2(1.0, 0);
+}
 
 void Game::Run() {
+  Setup();
   while (isRunning) {
     ProcessInput();
     Update();
@@ -67,13 +79,31 @@ void Game::ProcessInput() {
   }
 }
 
-void Game::Update() {}
+void Game::Update() {
+  while (!SDL_TICKS_PASSED(SDL_GetTicks(),
+                           msPreviousFrame + MILLISECONDS_PER_FRAME))
+    ;
+
+  msPreviousFrame = SDL_GetTicks();
+
+  playerPosition.x += playerVelocity.x;
+  playerPosition.y += playerVelocity.y;
+}
 
 void Game::Render() {
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
   SDL_RenderClear(renderer);
 
-  // TODO: Render all game objects..
+  // Draw a PNG texture
+  SDL_Surface *surface = IMG_Load("./assets/images/tank-tiger-right.png");
+  SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+  SDL_FreeSurface(surface);
+
+  SDL_Rect dstRect = {static_cast<int>(playerPosition.x),
+                      static_cast<int>(playerVelocity.y), 32, 32};
+
+  SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+  SDL_DestroyTexture(texture);
 
   SDL_RenderPresent(renderer);
 }
--- src/Game.h
+++ src/Game.h
@@ -4,15 +4,20 @@
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
 
+const int FPS = 60;
+const int MILLISECONDS_PER_FRAME = 1000 / FPS;
+
 class Game {
 private:
   bool isRunning;
+  int msPreviousFrame = 0;
   SDL_Window *window;
   SDL_Renderer *renderer;
 
 public:
   Game();
   ~Game();
+  void Setup();
   void Initialize();
   void Run();
   void ProcessInput();
