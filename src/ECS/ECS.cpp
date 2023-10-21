645a35f225a38549be3c798816d2fea3c443a97a
Miller Johnston <miller.johnston17@gmail.com>
2023-10-21 02:08:35 -0700

    ECS setup and TODO adds

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,3 +1,18 @@
 #include "ECS.h"
 
-// TODO: Implement all from ECS.h
+int Entity::GetId() const { return id; }
+
+void AddEntityToSystem(Entity entity) {
+  // TODO: Add the passed in Entity to this system
+}
+
+void RemoveEntityFromSystem(Entity entity) {
+  // TODO: Remove the passed in entity from this system
+}
+
+std::vector<Entity> GetAllSystemEntities() {
+  // TODO: Return all of the entities that this system is applying to
+}
+Signature &GetComponentSignature() {
+  // TODO: Return the Signature for a component
+}
--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -1,12 +1,54 @@
 #ifndef ECS_H
 #define ECS_H
+#include <bitset>
+#include <vector>
 
-class Component {};
+const unsigned int MAX_COMPONENTS = 32;
+typedef std::bitset<MAX_COMPONENTS> Signature;
 
-class Entity {};
+class BaseComponent {
+protected:
+  static int nextId;
+};
 
-class System {};
+template <typename TComponent> class Component : public BaseComponent {
+  static int GetId() {
+    static auto id = nextId++;
+    return id;
+  }
+};
+
+class Entity {
+private:
+  int id;
+
+public:
+  Entity(int id) : id(id){};
+  int GetId() const;
+};
+
+class System {
+private:
+  Signature componentSignature;
+  std::vector<Entity> entities;
+
+public:
+  System() = default;
+  ~System() = default;
+
+  void AddEntityToSystem(Entity entity);
+  void RemoveEntityFromSystem(Entity entity);
+
+  std::vector<Entity> GetAllSystemEntities() const;
+  Signature &GetComponentSignature() const;
+
+  template <typename TComponent> void RequireComponent();
+};
 
 class Registry {};
 
+template <typename TComponent> void System::RequireComponent() {
+  const auto componentId = Component<TComponent>::GetId();
+  componentSignature.set(componentId);
+}
 #endif
