6cdbde22008fc0cd8f9b4b629ac5bf187012dac9
Miller Johnston <miller.johnston17@gmail.com>
2023-10-23 00:44:03 -0700

    Added template functions for adding, removing components

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,9 +1,14 @@
 #include "ECS.h"
+#include "../Logger/Logger.h"
 
 #include <algorithm>
+#include <string>
+#include <utility>
 
+// ENTITY METHODS
 int Entity::GetId() const { return id; }
 
+// SYSTEM METHODS
 void System::AddEntityToSystem(Entity entity) { entities.push_back(entity); }
 
 void System::RemoveEntityFromSystem(Entity entity) {
@@ -16,6 +21,21 @@ void System::RemoveEntityFromSystem(Entity entity) {
 
 std::vector<Entity> System::GetAllSystemEntities() const { return entities; }
 
-const Signature &System::GetComponentSignature() const {
+const Signature& System::GetComponentSignature() const {
   return componentSignature;
+}
+
+// REGISTRY METHODS
+Entity Registry::CreateEntity() {
+  int entityId = numEntities++;
+  if (entityId >= entityComponentSignature.size()) {
+    entityComponentSignature.resize(entityId + 1);
+  }
+
+  Entity entityToCreate = Entity(entityId);
+  entitiesToBeAdded.insert(entityToCreate);
+
+  Logger::Log("Entity created with ID: " + std::to_string(entityId));
+
+  return entityToCreate;
 }
--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -1,7 +1,14 @@
 #ifndef ECS_H
 #define ECS_H
 
+#include "../Logger/Logger.h"
 #include <bitset>
+#include <cwchar>
+#include <set>
+#include <string>
+#include <strings.h>
+#include <typeindex>
+#include <unordered_map>
 #include <vector>
 
 const unsigned int MAX_COMPONENTS = 32;
@@ -45,19 +52,24 @@ public:
   void RemoveEntityFromSystem(Entity entity);
 
   std::vector<Entity> GetAllSystemEntities() const;
-  const Signature &GetComponentSignature() const;
+  const Signature& GetComponentSignature() const;
 
   template <typename TComponent> void RequireComponent();
 };
 
+class TPool {
+public:
+  virtual ~TPool() {}
+};
+
 // Pool class to track all the components in the Registry
-template <typename T> class Pool {
+template <typename T> class Pool : public TPool {
 private:
   std::vector<T> data;
 
 public:
   Pool(int size = 100) { Resize(size); }
-  ~Pool() = default;
+  virtual ~Pool() = default;
 
   bool IsEmpty() const { return data.empty(); }
   int GetSize() const { return data.size(); }
@@ -65,31 +77,120 @@ public:
   void Clear() { data.clear(); };
   void Add(T object) { data.push_back(object); }
   void Set(int index, T object) { data[index] = object; }
-  T &Get(int index) { return static_cast<T &>(data[index]); }
-  T &operator[](unsigned int index) { return data[index]; }
+  void RemoveAt(int index) { data.erase(data.begin() + index); }
+  T& Get(int index) { return static_cast<T&>(data[index]); }
+  T& operator[](unsigned int index) { return data[index]; }
 };
 
-// Registry class to handle the global state of entities
+// Registry class to handle the global state of entities, add systems, and
+// components
 class Registry {
 private:
   int numEntities = 0;
-  std::vector<Pool *> componentPools;
+
+  // Each pool contains data for a certain component type
+  // [vector index = componentId], [pool index = entityId]
+  std::vector<TPool*> componentPools;
+
+  // Signatures tell which components are active for an entity
+  // [vector index = entity id]
+  std::vector<Signature> entityComponentSignature;
+
+  // Map of active systems
+  // [index = system typeid]
+  std::unordered_map<std::type_index, System*> systems;
+
+  std::set<Entity> entitiesToBeAdded;
+  std::set<Entity> entitiesToBeDeleted;
 
 public:
   Registry() = default;
 
+  void Update();
+
+  // Entity Methods
   Entity CreateEntity();
   void KillEntity(Entity entity);
+
+  // System Methods
   void AddSystem();
   void RemoveSystem();
-  void AddComponent();
-  void RemoveComponent();
+  void HasSystem();
+  void GetSystem();
+  void AddEntityToSystem(Entity entity);
+
+  // Component Methods
+  template <typename T, typename... TArgs>
+  void AddComponent(Entity entity, TArgs&&... args);
+  template <typename T> void RemoveComponent(Entity entity);
+  template <typename T> bool HasComponent(Entity entity) const;
+  void GetComponent();
 };
 
-// Template function to implement the RequireComponent function in system
-// classes
+// Template functions
 template <typename TComponent> void System::RequireComponent() {
   const auto componentId = Component<TComponent>::GetId();
   componentSignature.set(componentId);
 }
+
+template <typename T, typename... TArgs>
+void Registry::AddComponent(Entity entity, TArgs&&... args) {
+  const auto componentId = Component<T>::GetId();
+  const auto entityId = entity.GetId();
+
+  if (componentId >= componentPools.size()) {
+    componentPools.resize(componentId + 1, nullptr);
+  }
+
+  if (!componentPools[componentId]) {
+    Pool<T>* newComponentPool = new Pool<T>();
+    componentPools[componentId] = newComponentPool;
+  }
+
+  Pool<T>* currentPool = componentPools[componentId];
+
+  if (entityId > currentPool->GetSize()) {
+    currentPool->Resize(numEntities);
+  }
+
+  // Create component of type T and pass in the parameters
+  T newComponent(std::forward<TArgs>(args)...);
+
+  // Add the new component to the entity within the current pool
+  currentPool->Set(entityId, newComponent);
+
+  // Flip the bit for the newly added component to "on"
+  entityComponentSignature[entityId].set(componentId);
+}
+
+template <typename T> void Registry::RemoveComponent(Entity entity) {
+  const auto componentId = Component<T>::GetId();
+  const auto entityId = entity.GetId();
+
+  if (componentId >= componentPools.size() || !componentPools[componentId]) {
+    Logger::Err("Attempting to remove component with ID: " +
+                std::to_string(componentId) +
+                "on the entity with ID: " + std::to_string(entityId));
+    return;
+  }
+
+  Pool<T>* currentPool = componentPools[componentId];
+
+  if (!currentPool[entityId]) {
+    Logger::Err("Trying to remove a component from an entity that does not "
+                "contain that component");
+    return;
+  }
+
+  currentPool->RemoveAt(entityId);
+  entityComponentSignature[entityId].set(componentId, false);
+}
+
+template <typename T> bool Registry::HasComponent(Entity entity) const {
+  const auto componentId = Component<T>::GetId();
+  const auto entityId = entity.GetId();
+
+  return entityComponentSignature[entityId].test(componentId);
+}
+
 #endif
