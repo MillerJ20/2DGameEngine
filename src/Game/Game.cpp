1fb32c88a49b6d192225aed6dc93469430b66927
Miller J <miller.johnston17@gmail.com>
2023-11-05 17:03:03 -0800

    Added system, refactor to fix seg fault (idk what caused it)

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -3,23 +3,16 @@
 #include "../Components/TransformComponent.h"
 #include "../ECS/ECS.h"
 #include "../Logger/Logger.h"
-#include "glm/fwd.hpp"
+#include "../Systems/MovementSystem.h"
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_keycode.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_surface.h>
-#include <SDL2/SDL_timer.h>
-#include <SDL2/SDL_video.h>
 #include <glm/glm.hpp>
 #include <iostream>
-#include <memory>
 
 Game::Game() {
   isRunning = false;
   registry = std::make_unique<Registry>();
-  Logger::Log("Game constructor Called!");
+  Logger::Log("Game constructor called!");
 }
 
 Game::~Game() { Logger::Log("Game destructor called!"); }
@@ -29,48 +22,24 @@ void Game::Initialize() {
     Logger::Err("Error initializing SDL.");
     return;
   }
-
   SDL_DisplayMode displayMode;
   SDL_GetCurrentDisplayMode(0, &displayMode);
   windowWidth = displayMode.w;
   windowHeight = displayMode.h;
-
   window =
       SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                        windowWidth, windowHeight, SDL_WINDOW_BORDERLESS);
   if (!window) {
-    std::cerr << "Error Creating SDL widnow." << std::endl;
+    Logger::Err("Error creating SDL window.");
     return;
   }
-
-  renderer = SDL_CreateRenderer(
-      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  renderer = SDL_CreateRenderer(window, -1, 0);
   if (!renderer) {
-    std::cerr << "Error creating SDL renderer" << std::endl;
+    Logger::Err("Error creating SDL renderer.");
     return;
   }
   SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-
   isRunning = true;
-}
-
-void Game::Setup() {
-  Entity tank = registry->CreateEntity();
-
-  tank.AddComponent<TransformComponent>(glm::vec2(10.0, 30.0),
-                                        glm::vec2(1.0, 1.0), 0.0);
-  tank.AddComponent<RigidBodyComponent>(glm::vec2(5.0, 5.0));
-  // tank.AddComponent<SpriteComponent>("./assets/images/tank.png");
-  tank.RemoveComponent<TransformComponent>();
-}
-
-void Game::Run() {
-  Setup();
-  while (isRunning) {
-    ProcessInput();
-    Update();
-    Render();
-  }
 }
 
 void Game::ProcessInput() {
@@ -89,34 +58,56 @@ void Game::ProcessInput() {
   }
 }
 
+void Game::Setup() {
+  // Add the systems that need to be processed in our game
+  registry->AddSystem<MovementSystem>();
+
+  // Create an entity
+  Entity tank = registry->CreateEntity();
+
+  // Add some components to that entity
+  tank.AddComponent<TransformComponent>(glm::vec2(10.0, 30.0),
+                                        glm::vec2(1.0, 1.0), 0.0);
+  tank.AddComponent<RigidBodyComponent>(glm::vec2(10.0, 50.0));
+}
+
 void Game::Update() {
-  // Only perform updates once per frame, wait if previous frame finished
-  // quickly (This caps the framerate)
+  // If we are too fast, waste some time until we reach the MILLISECS_PER_FRAME
   int timeToWait = MILLISECONDS_PER_FRAME - (SDL_GetTicks() - msPreviousFrame);
   if (timeToWait > 0 && timeToWait <= MILLISECONDS_PER_FRAME) {
     SDL_Delay(timeToWait);
   }
 
-  // Calculate the difference in time that has passed since last frame converted
-  // to seconds
-  // double deltaTime = (SDL_GetTicks() - msPreviousFrame) / 1000.0;
+  // The difference in ticks since the last frame, converted to seconds
+  double deltaTime = (SDL_GetTicks() - msPreviousFrame) / 1000.0;
 
-  // Store preiove frame time
+  // Store the "previous" frame time
   msPreviousFrame = SDL_GetTicks();
 
-  // TODO:
-  // MovementSystem.Update();
-  // CollisionSystem.Update();
+  // Update the registry to process the entities that are waiting to be
+  // created/deleted
+  registry->Update();
+
+  // Ask all the systems to update
+  registry->GetSystem<MovementSystem>().Update(deltaTime);
 }
 
 void Game::Render() {
   SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
-  msPreviousFrame = SDL_GetTicks();
   SDL_RenderClear(renderer);
 
-  // TODO:: Render game objects...
+  // TODO: Render game objects...
 
   SDL_RenderPresent(renderer);
+}
+
+void Game::Run() {
+  Setup();
+  while (isRunning) {
+    ProcessInput();
+    Update();
+    Render();
+  }
 }
 
 void Game::Destroy() {
